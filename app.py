b3ab89dc7750012c428b8369cf008e7c49a32868
Thu Nguyen <thunguy@me.com>
2020-11-03 08:46:32 -0800

    Add comments.

--- app.py
+++ app.py
@@ -12,10 +12,12 @@ def get_status():
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
 
+    # Define error messages
     tags_error = {'error': 'tags parameter is required'}
     sort_error = {'error': 'sortBy parameter is invalid'}
     direction_error = {'error': 'direction parameter is invalid'}
 
+    # Parse query params
     tags = request.args.get('tags', default=None, type=str)
     if not tags: return jsonify(tags_error), 400
 
@@ -27,12 +29,17 @@ def get_posts():
 
     tags = request.args.get('tags').strip().lower().split(',')
 
+    # For every tag in tags query param, fetch all posts that have specified tag listed in object
     data = [requests.get(f'https://api.hatchways.io/assessment/blog/posts?tag={tag}').json() for tag in tags]
+
+    # Flatten data
     posts = [p for d in data for v in d.values() for p in v]
 
+    # Filter out duplicate posts by 'id'
     ids = set()
     posts = [post for post in posts if post['id'] not in ids and (ids.add(post['id']) or True)]
 
+    # Sort and order results based on sortBy and direction parameters
     filter_by = lambda x: x['id'] if sort_by in ['id', ''] else x[sort_by]
     is_reversed = True if direction == 'desc' else False
     result = sorted(posts, key=filter_by, reverse=is_reversed)
